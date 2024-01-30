8e94bb9bcf679f6eb92ed242f0adf569510c68b1
a22908 <a22908@d1-08-03.dped.local>
2024-01-30 12:15:35 +0000

    Primeira commit da ficha trab 4

---
+++ src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
@@ -0,0 +1,65 @@
+package psi_11_o.m17b_fichatrabalho4;
+
+/**
+ *
+ * @author DiogoR
+ * @version 1.0
+ */
+
+public class Ponto2D {
+
+    private int x;
+    private int y;
+
+    public Ponto2D() {
+        x = 0;
+        y = 0;
+    }
+
+    public Ponto2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return returna o valor de x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x altera o valor de x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return returna o valor de y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y altera o valor de y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+     /**
+     * @return distancia returna o resultado do calculo da distancia
+     */
+    public double distancia(Ponto2D a) {
+        return Math.abs(Math.sqrt(Math.pow((getX() - a.getX()), 2) + Math.pow((getY() - a.getY()), 2)));
+    }
+
+    /**
+     * @return toString returna a string modificada 
+     */
+    @Override
+    public String toString() {
+        return "Ponto2D[" + getX() + "," + getY() + "]";
+    }
+}
